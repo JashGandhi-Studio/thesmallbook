0d8e685cd1341481c26c38d7af62352a229b8a14
Jash Gandhi <acimotreyothy@gmail.com>
2026-08-12 12:55:29 +0530

    Minor Fix!

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Bump CACHE_VERSION when you deploy changes.
    ============================================================ */
 
-const CACHE_VERSION = "tsb-v117";
+const CACHE_VERSION = "tsb-v118";
 const APP_SHELL = [
   "./",
   "./index.html",
